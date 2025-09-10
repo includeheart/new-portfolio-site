655eefa14241942fa205390695c440e668891d1f
Patrick Kernan <pkernan18@yahoo.com>
2025-09-10 09:57:12 -0400

    Update ProfileSection.tsx

--- app/components/ProfileSection.tsx
+++ app/components/ProfileSection.tsx
@@ -35,7 +35,7 @@ const ProfileSection = () => {
 							<path d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
 							<path d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
 						</svg>
-						<span className="text-sm text-gray-600 dark:text-gray-400">Tavares, FL</span>
+						<span className="text-sm text-gray-600 dark:text-gray-400">Barberton, OH</span>
 					</div>
 
 					{/* Social Links */}
